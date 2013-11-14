5b307db2f67b6ce4ed1b023a1327dfa3b79a165b
sai <sai@sai-300E4Z-300E5Z-300E7Z.(none)>
2013-11-14 12:13:03 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ssrk::Application.config.secret_token = 'db1be0d4186b100941a69035c7e72d152513ed39dee9bbdee45ab26d72f22c358601c0dfaacc56f5d58088910989828af9c32d39611f049211880f736cfd2bdf'
